39379c758408bd969ca20d340d8ddd66fcfa842f
OctopusSquared <oriolmon@gmail.com>
2026-05-05 19:30:36 +0200

    Modificar Dades.java i crear Adaptador.java

--- src/prog2/model/Dades.java
+++ src/prog2/model/Dades.java
@@ -19,10 +19,6 @@ public class Dades implements InDades {
     @Override
     public void afegirExemplar(String id, String titol, String autor, boolean admetPrestecLlarg) throws BiblioException {
         Exemplar exemplar = new Exemplar(id, titol, autor, admetPrestecLlarg);
-
-        if (llistaExemplars.contains(id)) {
-            throw new BiblioException("Aquest exemplar amb id " + id + " ja existeix.");
-        }
         llistaExemplars.afegir(exemplar);
     }
 
@@ -34,18 +30,16 @@ public class Dades implements InDades {
 
     @Override
     public void afegirUsuari(String email, String nom, String adreca, boolean esEstudiant) throws BiblioException {
-        if (llistaUsuaris.contains(email)) {
-            throw new BiblioException("Aquest correu " + email + " ja està en us.");
+        Usuari usuari;
+        if (esEstudiant) {
+            usuari = new Estudiant(email, nom, adreca);
         }
-    }
+        else {
+            usuari = new Professor(email, nom, adreca);
+        }
 
-    Usuari usuari;
-    if (esEstudiant){
-        usuari = new Estudiant(email, nom, adreca);
-    } else {
-        usuari = new Professor(email, nom, adreca);
+        llistaUsuaris.afegir(usuari);
     }
-    llistaUsuaris.afegir(usuari);
 
     @Override
     public ArrayList<Usuari> recuperaUsuaris() {
@@ -57,13 +51,13 @@ public class Dades implements InDades {
         Exemplar exemplar = llistaExemplars.getAt(exemplarPos);
         Usuari usuari = llistaUsuaris.getAt(usuariPos);
 
-        if (!exemplar.getDisponible()) {
+        if (!exemplar.isDisponible()) {
             throw new BiblioException("L'exemplar no està disponible.");
         }
         if (esLlarg && !exemplar.getAdmetPrestecLlarg()) {
             throw new BiblioException("L'exemplar no admet prestecs llargs.");
         }
-        if (tePrestecsEndarrerits(usuari)){
+        if (usuari.tePrestecsEndarrerits(usuari)){
             throw new BiblioException("L'usuari té prestecs endarrerits.");
         }
     }
