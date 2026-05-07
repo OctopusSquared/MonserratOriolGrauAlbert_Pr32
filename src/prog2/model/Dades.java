bc5284cdf63fa18371df7d2a50cf50b2455fe945
OctopusSquared <oriolmon@gmail.com>
2026-05-07 11:29:41 +0200

    Dades.java compila i no sembla tenir errors

--- src/prog2/model/Dades.java
+++ src/prog2/model/Dades.java
@@ -16,19 +16,16 @@ public class Dades implements InDades {
 
     }
 
-    @Override
     public void afegirExemplar(String id, String titol, String autor, boolean admetPrestecLlarg) throws BiblioException {
         Exemplar exemplar = new Exemplar(id, titol, autor, admetPrestecLlarg);
         llistaExemplars.afegir(exemplar);
     }
 
-    @Override
     public ArrayList<Exemplar>
     recuperaExemplars() {
         return llistaExemplars.getArrayList();
     }
 
-    @Override
     public void afegirUsuari(String email, String nom, String adreca, boolean esEstudiant) throws BiblioException {
         Usuari usuari;
         if (esEstudiant) {
@@ -41,24 +38,92 @@ public class Dades implements InDades {
         llistaUsuaris.afegir(usuari);
     }
 
-    @Override
     public ArrayList<Usuari> recuperaUsuaris() {
         return llistaUsuaris.getArrayList();
     }
 
-    @Override
     public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException {
         Exemplar exemplar = llistaExemplars.getAt(exemplarPos);
         Usuari usuari = llistaUsuaris.getAt(usuariPos);
+        Date dataActual = new Date();
+        Prestec prestec;
 
+        // Llançar les excepcions necessaries
+        //------------------------------------
+
+        // Fer un prestec d’un exemplar no disponible.
         if (!exemplar.isDisponible()) {
             throw new BiblioException("L'exemplar no està disponible.");
         }
-        if (esLlarg && !exemplar.getAdmetPrestecLlarg()) {
-            throw new BiblioException("L'exemplar no admet prestecs llargs.");
+        if (esLlarg) {
+            // Afegir un objecte de tipus PrestecLlarg per a un exemplar que no admet prestec llargs
+            if (!exemplar.getAdmetPrestecLlarg()) {
+                throw new BiblioException("L'exemplar no admet prestecs llargs.");
+            }
+            // Fer un prestec a un usuari que excedeix el seu limit de prestecs llargs
+            if (usuari.getMaxPrestecsLlargs() <= usuari.getNumPrestecsLlargs()) {
+                throw new BiblioException("l'usuari no pot excedeir el seu limit de prestecs llargs.");
+            }
         }
-        if (usuari.tePrestecsEndarrerits(usuari)){
-            throw new BiblioException("L'usuari té prestecs endarrerits.");
+        else {
+            // Fer un prestec a un usuari que excedeix el seu limit de prestecs normals
+            if (usuari.getMaxPrestecsNormals() <= usuari.getNumPrestecsNormals()) {
+                throw new BiblioException("l'usuari no pot excedeir el seu limit de prestecs normals.");
+            }
         }
+        // Recorrem la llista de prestecs per esbrinar si l'usuari té prestecs endarrerits
+        for (Prestec p : llistaPrestecs.getArrayList()) {
+            // Fer un prestec a un usuari que té prestecs endarrerits.
+            if (
+                    p.getUsuari().equals(usuari) &&
+                    p.getDataLimitRetorn().before(dataActual)
+            ) {
+                throw new BiblioException("L'usuari té prestecs endarrerits.");
+            }
+        }
+
+        // Afegir el Prestec a la llista
+        //-------------------------------
+        if (esLlarg) prestec = new PrestecLlarg(exemplar, usuari, dataActual);
+        else prestec = new PrestecNormal(exemplar, usuari, dataActual);
+
+        llistaPrestecs.afegir(prestec);
+
+        // Marcar l'exemplar com "No disponible"
+        exemplar.setDisponible(false);
+    }
+
+    public void retornarPrestec(int position) throws BiblioException {
+        Prestec prestec;
+
+        // Excepcions necessaries
+        if (llistaPrestecs.getSize() < position) {
+            throw new BiblioException("No es pot retornar un prestec ja retornat");
+        }
+
+        // Retornar el prestec
+        prestec = llistaPrestecs.getAt(position);
+        llistaPrestecs.esborrar(prestec);
+
+        // Marcar l'exemplar com "Disponible"
+        prestec.getExemplar().setDisponible(true);
+    }
+
+
+    public ArrayList<Prestec> recuperaPrestecs() {
+        return llistaPrestecs.getArrayList();
+    }
+
+    public ArrayList<Prestec> recuperaPrestecsNoRetornats() {
+        ArrayList<Prestec> prestecs = new ArrayList<>();
+
+        // Recorrem la llista de Prestecs i guardem aguels que estiguin endarrerits
+        for (Prestec p : llistaPrestecs.getArrayList()) {
+            if (p.getDataLimitRetorn().before(new Date())) {
+                prestecs.add(p);
+            }
+        }
+
+        return prestecs;
     }
 }
