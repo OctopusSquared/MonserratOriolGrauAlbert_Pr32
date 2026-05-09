412edaa62fa0447e8f6935745bb6d3459d3f8c9a
Grau21 <albertgrauperez@gmail.com>
2026-05-09 22:53:58 +0200

    Modi Exemplar.java

--- src/prog2/model/Exemplar.java
+++ src/prog2/model/Exemplar.java
@@ -1,63 +1,47 @@
 package prog2.model;
 
-public class Exemplar implements InExemplar {
-    // Atributs
-    //------------
+import java.io.Serializable;
+
+public class Exemplar implements InExemplar, Serializable {
     private String id;
     private String autor;
     private String titol;
     private boolean admetPrestecLlarg;
     private boolean disponible;
 
-    // Constructors
-    //---------------
-
     /**
      * Constructor per paràmetres de la classe.
-     * @param id
-     * @param autor
-     * @param titol
-     * @param admetPrestecLlarg
+     * @param id identificador numèric
+     * @param titol títol de l'exemplar
+     * @param autor autor de l'exemplar
+     * @param admetPrestecLlarg indica si admet préstec de llarg termini
      */
-    public Exemplar (String id, String autor, String titol, boolean admetPrestecLlarg){
+    public Exemplar(String id, String titol, String autor, boolean admetPrestecLlarg) {
         this.id = id;
-        this.autor = autor;
         this.titol = titol;
+        this.autor = autor;
         this.admetPrestecLlarg = admetPrestecLlarg;
         disponible = true;
     }
 
-    // Getters i Setters.
-    //---------------------
-
+    // Getters i Setters
     public void setId(String id) { this.id = id; }
     public String getId() { return id; }
-
     public void setTitol(String titol) { this.titol = titol; }
     public String getTitol() { return titol; }
-
     public void setAutor(String autor) { this.autor = autor; }
     public String getAutor() { return autor; }
-
     public void setAdmetPrestecLlarg(boolean admetPrestecLlarg) { this.admetPrestecLlarg = admetPrestecLlarg; }
     public boolean getAdmetPrestecLlarg() { return admetPrestecLlarg; }
-
     public void setDisponible(boolean disponible) { this.disponible = disponible; }
     public boolean isDisponible() { return disponible; }
 
-    // Mètodes
-    //---------
-
-    /**
-     * Retorna un String amb la informació de l'Exemplar.
-     * @return String
-     */
     @Override
     public String toString() {
         return "Id=" + id +
                 ", Titol=" + titol +
                 ", Autor=" + autor +
-                ", Admet Prestec Llarg=" + (admetPrestecLlarg? "true":"false") +
-                ", Disponible=" + (disponible? "true":"false");
+                ", Admet Prestec Llarg=" + (admetPrestecLlarg ? "true" : "false") +
+                ", Disponible=" + (disponible ? "true" : "false");
     }
 }
