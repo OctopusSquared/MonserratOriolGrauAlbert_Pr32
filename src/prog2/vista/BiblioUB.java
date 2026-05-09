793f613cc096654c33b1145a8fcad2c408d0fca3
Grau21 <albertgrauperez@gmail.com>
2026-05-09 23:00:41 +0200

    Modi BiblioUB.java

--- src/prog2/vista/BiblioUB.java
+++ src/prog2/vista/BiblioUB.java
@@ -1,58 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prog2.vista;
 
 import java.util.List;
 import java.util.Scanner;
 import prog2.adaptador.Adaptador;
 
-/**
- *
- * @author dortiz
- */
 public class BiblioUB {
-    
-    // Declarem les constants del menu principal
+
     static private enum OpcionsMenuPrincipal {
         MENU_PRINCIPAL_EXEMPLARS,
         MENU_PRINCIPAL_USUARIS,
         MENU_PRINCIPAL_PRESTECS,
         MENU_PRINCIPAL_SAVE,
         MENU_PRINCIPAL_LOAD,
-        MENU_PRINCIPAL_EXIT};
-    
-    // Declarem descripcions personalitzades per a les opcions del menú principal
-    static private String[] descMenuPrincipal={"Gestió Exemplars",
-                                               "Gestió Usuaris",
-                                               "Gestió Prestecs",
-                                               "Guardar Dades",
-                                               "Recuperar Dades",
-                                               "Sortir"};
+        MENU_PRINCIPAL_EXIT
+    }
+
+    static private String[] descMenuPrincipal = {"Gestió Exemplars",
+                                                 "Gestió Usuaris",
+                                                 "Gestió Prestecs",
+                                                 "Guardar Dades",
+                                                 "Recuperar Dades",
+                                                 "Sortir"};
 
     static private enum OpcionsMenuGestioExemplars {
         MENU_GESTIO_EXEMPLARS_ADD,
         MENU_GESTIO_EXEMPLARS_VIEW,
         MENU_GESTIO_EXEMPLARS_EXIT
-    };
-    
-    // Declarem descripcions personalitzades per a les opcions del menú principal
-    static private String[] descMenuGestioExemplars ={"Afegir Exemplar",
-                                                      "Visualitzar Exemplars",
-                                                      "Sortir"};
+    }
+
+    static private String[] descMenuGestioExemplars = {"Afegir Exemplar",
+                                                       "Visualitzar Exemplars",
+                                                       "Sortir"};
 
     static private enum OpcionsMenuGestioClients {
         MENU_GESTIO_USUARIS_ADD,
         MENU_GESTIO_USUARIS_VIEW,
         MENU_GESTIO_USUARIS_EXIT
-    };
-    
-    // Declarem descripcions personalitzades per a les opcions del menú principal
-    static private String[] descMenuGestioUsuaris ={"Afegir Usuari",
-                                                    "Visualitzar Usuaris",
-                                                    "Sortir"};
+    }
+
+    static private String[] descMenuGestioUsuaris = {"Afegir Usuari",
+                                                     "Visualitzar Usuaris",
+                                                     "Sortir"};
 
     static private enum OpcionsMenuGestioPrestecs {
         MENU_GESTIO_PRESTECS_ADD,
@@ -60,165 +48,252 @@ public class BiblioUB {
         MENU_GESTIO_PRESTECS_VIEW,
         MENU_GESTIO_PRESTECS_VIEW_URG,
         MENU_GESTIO_PRESTECS_EXIT
-    };
-    
-    // Declarem descripcions personalitzades per a les opcions del menú principal
-    static private String[] descMenuGestioPrestecs ={"Afegir Prestec",
-                                                     "Retornar Prestec",
-                                                     "Visualitzar Prestecs",
-                                                     "Visualitzar Prestecs no Retornats",
-                                                     "Sortir"};
+    }
 
-    
-    /** Adaptador de l'aplicació */
+    static private String[] descMenuGestioPrestecs = {"Afegir Prestec",
+                                                      "Retornar Prestec",
+                                                      "Visualitzar Prestecs",
+                                                      "Visualitzar Prestecs no Retornats",
+                                                      "Sortir"};
+
     private Adaptador adaptador;
-    
-    /* Constructor*/
+
     public BiblioUB() {
         adaptador = new Adaptador();
     }
-     
-    public void gestioBiblioUB() {
-        // Creem un objecte per llegir des del teclat
-        Scanner sc = new Scanner(System.in);
-        
-        // Creem l'objecte per al menú. Li passem com a primer paràmetre el nom del menú
-        Menu<OpcionsMenuPrincipal> menu = new Menu<>("Menu principal", OpcionsMenuPrincipal.values());
 
-        // Assignem la descripció de les opcions
+    public void gestioBiblioUB() {
+        Scanner sc = new Scanner(System.in);
+        Menu<OpcionsMenuPrincipal> menu = new Menu<>("Menu principal", OpcionsMenuPrincipal.values());
         menu.setDescripcions(descMenuPrincipal);
-        
+
         OpcionsMenuPrincipal opcio;
         do {
-            // Mostrem les opcions del menú i demanem una opció
             menu.mostrarMenu();
             opcio = menu.getOpcio(sc);
 
-            // Fem les accions necessàries per a la opció triada
-            switch(opcio) {
+            switch (opcio) {
                 case MENU_PRINCIPAL_EXEMPLARS:
-                    // Mostra el menú per a la gestió d'exemplars
                     menuGestioExemplars(sc);
                     break;
-
                 case MENU_PRINCIPAL_USUARIS:
-                    // Mostra el menú per a la gestió d'usuaris
                     menuGestioUsuaris(sc);
                     break;
-
                 case MENU_PRINCIPAL_PRESTECS:
-                    // Mostra el menú per a la gestió de prestecs
                     menuGestioPrestecs(sc);
                     break;
-
                 case MENU_PRINCIPAL_SAVE:
-                    // Guardar dades
-                    String dstFile = getFilePath(sc,false); // Obtenir el fitxer de sortida
-                    if(dstFile != null) {
-                        // Guardar les dades al fitxer triat
-                        try {
-                             this.adaptador.guardaDades(dstFile);
-                             System.err.println("Dades guardades");
-                        } catch (BiblioException ex) {
-                            System.out.println("Error guardant les dades: " + ex.getMessage());
-                        }
-                    }                   
+                    guardarDades(sc);
                     break;
                 case MENU_PRINCIPAL_LOAD:
-                    // Carregar dades                   
-                    String srcFile = getFilePath(sc,false); // Obtenir el fitxer d'entrada
-                    if(srcFile != null) {
-                        // Carregar les dades del fitxer triat
-                        try {
-                             this.adaptador.carregaDades(srcFile);
-                             System.err.println("Dades carregades");
-                        } catch(BiblioException ex) {
-                            System.out.println("Error carregant les dades." + ex.getMessage());
-                        }
-                    }     
+                    carregarDades(sc);
                     break;
                 case MENU_PRINCIPAL_EXIT:
-                    // Sortir      1
-                    System.err.println("Sortint de l'aplicació...");
+                    System.out.println("Sortint de l'aplicació...");
                     break;
             }
-        } while(opcio != OpcionsMenuPrincipal.MENU_PRINCIPAL_EXIT);
+        } while (opcio != OpcionsMenuPrincipal.MENU_PRINCIPAL_EXIT);
     }
-    
+
     private void menuGestioExemplars(Scanner sc) {
+        Menu<OpcionsMenuGestioExemplars> menu = new Menu<>("Gestió Exemplars", OpcionsMenuGestioExemplars.values());
+        menu.setDescripcions(descMenuGestioExemplars);
+        OpcionsMenuGestioExemplars opcio;
+        do {
+            menu.mostrarMenu();
+            opcio = menu.getOpcio(sc);
+            switch (opcio) {
+                case MENU_GESTIO_EXEMPLARS_ADD:
+                    afegirExemplar(sc);
+                    break;
+                case MENU_GESTIO_EXEMPLARS_VIEW:
+                    showList("Exemplars", adaptador.llistaExemplarsToLlistaString());
+                    break;
+                case MENU_GESTIO_EXEMPLARS_EXIT:
+                    break;
+            }
+        } while (opcio != OpcionsMenuGestioExemplars.MENU_GESTIO_EXEMPLARS_EXIT);
     }
-    
-    /**
-     * Afegir un nou article
-     * @param sc
-     */
-    
-    private void afegirExemplar(Scanner sc){
+
+    private void afegirExemplar(Scanner sc) {
+        System.out.print("Identificador: ");
+        String id = sc.nextLine();
+        System.out.print("Títol: ");
+        String titol = sc.nextLine();
+        System.out.print("Autor: ");
+        String autor = sc.nextLine();
+        System.out.print("Admet préstec llarg (true/false): ");
+        boolean admetLlarg = sc.nextBoolean();
+        sc.nextLine(); // consumir salt de línia
+
+        try {
+            adaptador.afegirExemplar(id, titol, autor, admetLlarg);
+            System.out.println("Exemplar afegit correctament.");
+        } catch (BiblioException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     private void menuGestioUsuaris(Scanner sc) {
+        Menu<OpcionsMenuGestioClients> menu = new Menu<>("Gestió Usuaris", OpcionsMenuGestioClients.values());
+        menu.setDescripcions(descMenuGestioUsuaris);
+        OpcionsMenuGestioClients opcio;
+        do {
+            menu.mostrarMenu();
+            opcio = menu.getOpcio(sc);
+            switch (opcio) {
+                case MENU_GESTIO_USUARIS_ADD:
+                    afegirUsuari(sc);
+                    break;
+                case MENU_GESTIO_USUARIS_VIEW:
+                    showList("Usuaris", adaptador.llistaUsuarisToLlistaString());
+                    break;
+                case MENU_GESTIO_USUARIS_EXIT:
+                    break;
+            }
+        } while (opcio != OpcionsMenuGestioClients.MENU_GESTIO_USUARIS_EXIT);
     }
-    
-    /**
-     * Afegir un nou usuari
-     * @param sc
-     */
-    
-    private void afegirUsuari(Scanner sc){
+
+    private void afegirUsuari(Scanner sc) {
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Nom: ");
+        String nom = sc.nextLine();
+        System.out.print("Adreça: ");
+        String adreca = sc.nextLine();
+        System.out.print("És estudiant? (true/false): ");
+        boolean esEstudiant = sc.nextBoolean();
+        sc.nextLine();
+
+        try {
+            adaptador.afegirUsuari(email, nom, adreca, esEstudiant);
+            System.out.println("Usuari afegit correctament.");
+        } catch (BiblioException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     private void menuGestioPrestecs(Scanner sc) {
-    }
-    
-    /**
-     * Afegir un nou prestec
-     * @param sc
-     */
-    
-    private void afegirPrestec(Scanner sc){
+        Menu<OpcionsMenuGestioPrestecs> menu = new Menu<>("Gestió Prestecs", OpcionsMenuGestioPrestecs.values());
+        menu.setDescripcions(descMenuGestioPrestecs);
+        OpcionsMenuGestioPrestecs opcio;
+        do {
+            menu.mostrarMenu();
+            opcio = menu.getOpcio(sc);
+            switch (opcio) {
+                case MENU_GESTIO_PRESTECS_ADD:
+                    afegirPrestec(sc);
+                    break;
+                case MENU_GESTIO_PRESTECS_REMOVE:
+                    retornarPrestec(sc);
+                    break;
+                case MENU_GESTIO_PRESTECS_VIEW:
+                    showList("Tots els préstecs", adaptador.llistaPrestecsToLlistaString());
+                    break;
+                case MENU_GESTIO_PRESTECS_VIEW_URG:
+                    showList("Préstecs no retornats", adaptador.llistaPrestecsNoRetornatsToLlistaString());
+                    break;
+                case MENU_GESTIO_PRESTECS_EXIT:
+                    break;
+            }
+        } while (opcio != OpcionsMenuGestioPrestecs.MENU_GESTIO_PRESTECS_EXIT);
     }
 
-    private void cancelarPrestec(Scanner sc){
+    private void afegirPrestec(Scanner sc) {
+        // Mostrar exemplars disponibles
+        List<String> exemplars = adaptador.llistaExemplarsToLlistaString();
+        if (exemplars.isEmpty()) {
+            System.out.println("No hi ha exemplars.");
+            return;
+        }
+        showList("Exemplars", exemplars);
+        System.out.print("Tria l'índex de l'exemplar: ");
+        int exemplarPos = sc.nextInt();
+        sc.nextLine();
+
+        List<String> usuaris = adaptador.llistaUsuarisToLlistaString();
+        if (usuaris.isEmpty()) {
+            System.out.println("No hi ha usuaris.");
+            return;
+        }
+        showList("Usuaris", usuaris);
+        System.out.print("Tria l'índex de l'usuari: ");
+        int usuariPos = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Préstec llarg? (true/false): ");
+        boolean esLlarg = sc.nextBoolean();
+        sc.nextLine();
+
+        try {
+            adaptador.afegirPrestec(exemplarPos, usuariPos, esLlarg);
+            System.out.println("Préstec creat correctament.");
+        } catch (BiblioException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
-     /**
-     * Mostra una llista d'objectes
-     * @param title Títol a posar com a capçalera
-     * @param lines Llista d'objectes per mostrar
-     */
+    private void retornarPrestec(Scanner sc) {
+        List<String> prestecs = adaptador.llistaPrestecsToLlistaString();
+        if (prestecs.isEmpty()) {
+            System.out.println("No hi ha préstecs.");
+            return;
+        }
+        showList("Préstecs", prestecs);
+        System.out.print("Tria l'índex del préstec a retornar: ");
+        int pos = sc.nextInt();
+        sc.nextLine();
+
+        try {
+            adaptador.retornarPrestec(pos);
+            System.out.println("Préstec retornat correctament.");
+        } catch (BiblioException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void guardarDades(Scanner sc) {
+        String path = getFilePath(sc, false);
+        if (path != null) {
+            try {
+                adaptador.guardaDades(path);
+                System.out.println("Dades guardades.");
+            } catch (BiblioException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private void carregarDades(Scanner sc) {
+        String path = getFilePath(sc, true);
+        if (path != null) {
+            try {
+                adaptador.carregaDades(path);
+                System.out.println("Dades carregades.");
+            } catch (BiblioException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
     private void showList(String title, List<String> lines) {
         System.out.println("============================================");
         System.out.println(title);
         System.out.println("============================================");
         int i = 0;
-        for(String l : lines) {
+        for (String l : lines) {
             System.out.println("\t[" + (i++) + "] " + l);
         }
         System.out.println("============================================");
     }
 
-
-    /**
-     * Demana el camí d'un fitxer
-     * @param sc Objecte per a la lectura de dades de teclat
-     * @param mustExist Exigeix que el fitxer existeixi (True) o no (False)
-     * @return Ruta al fitxer entrada per l'usuari o null si s'ha cancelat
-     */
     private String getFilePath(Scanner sc, boolean mustExist) {
-        String filePath = null;
-
-        // Mostrar el missatge demanant la entrada
-        System.out.println("Entra ruta completa fitxer (o ENTER per ometre):");
-
-            // Llegim la ruta del fitxer
-            filePath = sc.nextLine();
-
-            // Si la ruta està buida retornem un null
-            if(filePath.isEmpty()) {
-                return null;
-            }
-
+        System.out.print("Entra ruta completa del fitxer (ENTER per ometre): ");
+        String filePath = sc.nextLine();
+        if (filePath.isEmpty()) {
+            return null;
+        }
+        // Si mustExist és true, es podria comprovar l'existència, però ho deixem simple
         return filePath;
     }
-
 }
