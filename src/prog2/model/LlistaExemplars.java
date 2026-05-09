da5f32938257d99eab952c9efda47ec891799d1c
Grau21 <albertgrauperez@gmail.com>
2026-05-09 22:58:32 +0200

    Modi LlistaExemplars.java

--- src/prog2/model/LlistaExemplars.java
+++ src/prog2/model/LlistaExemplars.java
@@ -5,23 +5,29 @@ import java.util.Iterator;
 import java.io.Serializable;
 
 public class LlistaExemplars extends Llista<Exemplar> implements Serializable {
+
     @Override
-    /**
-     * Afegir element a la llista. Afegeix l'Exemlar t a la llista
-     */
     public void afegir(Exemplar t) throws BiblioException {
         String id_t = t.getId();
-
-        // Recorrem la llista amb iteradors
         Iterator<Exemplar> it = llista.iterator();
         while (it.hasNext()) {
             Exemplar e = it.next();
-            if (e.getId() == id_t) {
+            if (e.getId().equals(id_t)) {
                 throw new BiblioException("No es poden afegir dos exemplars amb el mateix identificador");
             }
         }
-
-        // Si no s'han trobat dos exemplars amb el mateix id, es pot afegir el nou exemplar
         llista.add(t);
+    }
+
+    /**
+     * Retorna true si hi ha un exemplar amb l'id donat.
+     */
+    public boolean contains(String id) {
+        for (Exemplar e : llista) {
+            if (e.getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
