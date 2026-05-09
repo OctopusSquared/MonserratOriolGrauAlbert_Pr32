7bca3b0017e5088f978ef262725ddfedac5cfbb2
Grau21 <albertgrauperez@gmail.com>
2026-05-09 22:55:32 +0200

    Modi Usuari.java

--- src/prog2/model/Usuari.java
+++ src/prog2/model/Usuari.java
@@ -1,32 +1,19 @@
 package prog2.model;
 
-public abstract class Usuari implements InUsuari {
-    
-    // Atributs
-    //------------
+import java.io.Serializable;
 
+public abstract class Usuari implements InUsuari, Serializable {
     private String email;
     private String nom;
     private String adreca;
 
-    // Constructors
-    //---------------
-
-    /**
-     * Constructor per paràmetres de la classe.
-     * @param email
-     * @param nom
-     * @param adreca
-     */
     public Usuari(String email, String nom, String adreca) {
         this.email = email;
         this.nom = nom;
         this.adreca = adreca;
     }
 
-    // Getters i Setters.
-    //---------------------
-    
+    // Getters i Setters (sense canvis)
     public void setEmail(String email) { this.email = email; }
     public String getEmail() { return this.email; }
     public void setNom(String nom) { this.nom = nom; }
@@ -34,13 +21,6 @@ public abstract class Usuari implements InUsuari {
     public void setAdreca(String adreca) { this.adreca = adreca; }
     public String getAdreca() { return this.adreca; }
 
-    // Mètodes
-    //---------
-
-    /**
-     * Retorna un String amb la informació de l'Usuari.
-     * @return
-     */
     @Override
     public String toString() {
         return "Email=" + this.email +
