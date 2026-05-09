7c8370c5351dba4e9fa8b5387f37ae01aa49f4b7
Grau21 <albertgrauperez@gmail.com>
2026-05-09 22:57:10 +0200

    Modi Prestec.java

--- src/prog2/model/Prestec.java
+++ src/prog2/model/Prestec.java
@@ -1,41 +1,21 @@
 package prog2.model;
 
-public abstract class Prestec implements InPrestec {
+import java.io.Serializable;
 
-    // Atributs
-    //------------
-
+public abstract class Prestec implements InPrestec, Serializable {
     private Exemplar exemplar;
     private Usuari usuari;
 
-    // Constructors
-    //---------------
-
-    /**
-     * Constructor per paràmetres de la classe.
-     * @param exemplar
-     * @param usuari
-     */
     public Prestec(Exemplar exemplar, Usuari usuari) {
         this.exemplar = exemplar;
         this.usuari = usuari;
     }
-
-    // Getters i Setters.
-    //---------------------
 
     public void setExemplar(Exemplar exemplar) { this.exemplar = exemplar; }
     public Exemplar getExemplar() { return exemplar; }
     public void setUsuari(Usuari usuari) { this.usuari = usuari; }
     public Usuari getUsuari() { return usuari; }
 
-    // Mètodes
-    //---------
-
-    /**
-     * Retorna un String amb la informació de l'Estudiant.
-     * @return String
-     */
     @Override
     public String toString() {
         return "Exemplar=" + exemplar.getTitol() +
