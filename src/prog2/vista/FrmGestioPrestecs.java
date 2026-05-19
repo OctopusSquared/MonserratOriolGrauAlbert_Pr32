f8f14d7c02bea09ce6c63ff1b8e4708d4164314a
Grau21 <albertgrauperez@gmail.com>
2026-05-19 12:12:06 +0200

    Create FrmGestioPrestecs.java

---
+++ src/prog2/vista/FrmGestioPrestecs.java
@@ -0,0 +1,9 @@
+package Vista;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FrmGestioPrestecs extends JDialog {
+
+    private final ControladorGUI ctrl;
+    private final DefaultListModel<String> model
